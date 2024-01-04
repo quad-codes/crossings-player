a54b688cba50eb36f184ff0571d39a19092c4ca5
Pavlos Vinieratos <pvinis@gmail.com>
2024-01-04 13:52:44 +0200

    almost batman

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
-import "../global.css";
-import { Slot } from "expo-router";
+import "../global.css"
+import { Slot } from "expo-router"
 
 export default function Layout() {
-  return <Slot />;
+	return <Slot />
 }
