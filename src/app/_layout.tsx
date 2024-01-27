5d69be6ad0bfa8a94412846aa9a353168c6b4b5c
Pavlos Vinieratos <pvinis@gmail.com>
2024-01-27 16:06:04 +0200

    aha

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,11 @@
+import { Providers } from "@/utils/Providers"
 import "../global.css"
 import { Slot } from "expo-router"
 
 export default function Layout() {
-	return <Slot />
+	return (
+		<Providers>
+			<Slot />
+		</Providers>
+	)
 }
--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,7 +1,12 @@
 import { createClient, AnalyticsProvider } from "@segment/analytics-react-native"
 import { PropsWithChildren } from "react"
 
-const segmentClient = createClient({ writeKey: "SEGMENT_API_KEY" })
+
+const segmentClient = createClient({
+	writeKey: process.env.EXPO_PUBLIC_API_URL,
+	trackAppLifecycleEvents: true,
+	trackDeepLinks: true,
+})
 
 export function Providers({ children }: PropsWithChildren) {
 	return <AnalyticsProvider client={segmentClient}>{children}</AnalyticsProvider>
