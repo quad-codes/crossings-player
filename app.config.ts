4aa0c33c9a5d8bb9f94eab7cb71fbb34b9d8bf2b
Pavlos Vinieratos <pvinis@gmail.com>
2024-02-04 01:55:42 +0000

    ok

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,20 @@
 import "ts-node/register" // this helps to import TypeScript files
 import { ExpoConfig } from "expo/config"
+import v from "./versions.json"
 
 const config: ExpoConfig = {
-	name: "crossings",
+	name: "Καθημερινά Σταυρόλεξα",
 	slug: "crossings",
 	scheme: "crossings",
+	version: v.version,
 	icon: "./assets/icon.png",
 	ios: {
 		bundleIdentifier: "is.pvin.crossings",
+		buildNumber: String(v.build),
 	},
 	android: {
 		package: "is.pvin.crossings",
+		versionCode: v.build,
 	},
 	userInterfaceStyle: "automatic",
 	plugins: [["expo-router", { origin: "https://n" }]],
