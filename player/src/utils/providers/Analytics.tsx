db353773cf8a323698512f9135b6be1498eb468d
Pavlos Vinieratos <pvinis@gmail.com>
2024-06-20 18:18:14 +0300

    some analytics

--- player/src/utils/providers/Analytics.tsx
+++ player/src/utils/providers/Analytics.tsx
@@ -1,9 +1,13 @@
 import PostHog, { PostHogProvider } from "posthog-react-native"
 import { PropsWithChildren } from "react"
+import { Platform } from "react-native"
+import { modelName, osName, osVersion } from "expo-device"
 
 const client = new PostHog(process.env.EXPO_PUBLIC_POSTHOG_API_KEY, {
 	host: "https://eu.i.posthog.com",
 })
+
+client.register({ platform: Platform.OS, os: osName, os_version: osVersion, device: modelName })
 
 export function Analytics({ children }: PropsWithChildren) {
 	return <PostHogProvider client={client}>{children}</PostHogProvider>
