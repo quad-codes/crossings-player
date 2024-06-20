4d5ecb224e647f7975a56d8ec80246cce709e838
Pavlos Vinieratos <pvinis@gmail.com>
2024-06-20 18:23:03 +0300

    dev

--- player/src/utils/providers/Analytics.tsx
+++ player/src/utils/providers/Analytics.tsx
@@ -7,7 +7,13 @@ const client = new PostHog(process.env.EXPO_PUBLIC_POSTHOG_API_KEY, {
 	host: "https://eu.i.posthog.com",
 })
 
-client.register({ platform: Platform.OS, os: osName, os_version: osVersion, device: modelName })
+client.register({
+	platform: Platform.OS,
+	os: osName,
+	os_version: osVersion,
+	device: modelName,
+	is_dev: __DEV__,
+})
 
 export function Analytics({ children }: PropsWithChildren) {
 	return <PostHogProvider client={client}>{children}</PostHogProvider>
