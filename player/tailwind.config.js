c0842716f13d7904f678bde5318cc6412cc020a1
Pavlos Vinieratos <pvinis@gmail.com>
2024-07-07 16:11:45 +0300

    use just tw config file

--- player/tailwind.config.js
+++ player/tailwind.config.js
@@ -1,20 +1,56 @@
+const { reduce } = require("lodash")
+
+/**
+ * @typedef {"background" | "on-background" | "surface-not-started" | "surface-in-progress" | "surface-done" | "on-surface"} ColorName
+ * @typedef {Record<ColorName, string>} Colors
+ */
+
+/** @type Record<"light"|"dark", Colors> */
+const colors = {
+	light: {
+		background: "white",
+		"on-background": "black",
+		"surface-not-started": "#6A0DAD",
+		"surface-in-progress": "#A76ACD",
+		"surface-done": "#DCC1E9",
+		"on-surface": "white",
+	},
+	dark: {
+		background: "black",
+		"on-background": "white",
+		"surface-not-started": "green",
+		"surface-in-progress": "yellow",
+		"surface-done": "red",
+		"on-surface": "white",
+	},
+}
+
+/** @type Record<string,string> */
+const initForLight = {}
+/** @type Record<string,string> */
+const initForDark = {}
+
 module.exports = {
 	content: ["./src/**/*.{ts,tsx}"],
 	theme: {
 		extend: {
 			colors: {
-				background: "white",
-				"on-background": "black",
-				"surface-not-started": "#6A0DAD",
-				"surface-in-progress": "#A76ACD",
-				"surface-done": "#DCC1E9",
-				"on-surface": "white",
-				"dark-background": "black",
-				"dark-on-background": "white",
-				"dark-surface-not-started": "green",
-				"dark-surface-in-progress": "yellow",
-				"dark-surface-done": "red",
-				"dark-on-surface": "white",
+				...reduce(
+					colors.light,
+					(acc, value, key) => {
+						acc[key] = value
+						return acc
+					},
+					initForLight,
+				),
+				...reduce(
+					colors.dark,
+					(acc, value, key) => {
+						acc[`dark-${key}`] = value
+						return acc
+					},
+					initForDark,
+				),
 				t: "red",
 			},
 			fontFamily: { "mono-base": "Iosevka" },
