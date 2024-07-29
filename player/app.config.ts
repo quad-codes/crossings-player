e5099a3e17a24637b79c23d0a756c5ea4bed2455
Pavlos Vinieratos <pvinis@gmail.com>
2024-07-29 18:02:25 +0300

    trial for multi langs

--- player/app.config.ts
+++ player/app.config.ts
@@ -1,28 +1,32 @@
 import { ExpoConfig, ConfigContext } from "expo/config"
 import v from "./version.json"
+import loc from "./loc.json"
 
 const IS_DEV = process.env.APP_VARIANT === "development"
+
+const L = (process.env.L as "el" | "he") ?? "el"
+const LID = loc[L].id
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
 
-	name: IS_DEV ? "Καθημερινά Παιχνίδια (DEV)" : "Καθημερινά Παιχνίδια",
-	slug: "crossings",
-	scheme: "crossings",
+	name: IS_DEV ? `${loc[L].appName} (DEV)` : loc[L].appName,
+	slug: `crossings-${LID}`,
+	scheme: `crossings-${LID}`,
 	version: v.version,
-	icon: "./assets/app-icon.png",
+	icon: `./assets/app-icon-${LID}.png`,
 	ios: {
-		bundleIdentifier: IS_DEV ? "codes.quad.crossings.dev" : "codes.quad.crossings",
+		bundleIdentifier: IS_DEV ? `codes.quad.crossings.${LID}.dev` : `codes.quad.crossings.${LID}`,
 		buildNumber: String(v.build),
 		config: { usesNonExemptEncryption: false },
 	},
 	android: {
-		package: IS_DEV ? "codes.quad.crossings.dev" : "codes.quad.crossings",
+		package: IS_DEV ? `codes.quad.crossings.${LID}.dev` : `codes.quad.crossings.${LID}`,
 		versionCode: v.build,
 	},
 	userInterfaceStyle: "automatic",
 	splash: {
-		image: "./assets/splash-screen.png",
+		image: `./assets/splash-screen-${LID}.png`,
 		backgroundColor: "#150050",
 	},
 	plugins: [
@@ -32,8 +36,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	],
 
 	extra: {
-		eas: { projectId: "99375595-9000-4912-bb9a-1258a6ed9c0f" },
-		pvinis: {jsbuild: v.jsbuild}
+		eas: { projectId: loc[L].easProjectId },
+		pvinis: { jsbuild: v.jsbuild },
 	},
 
 	runtimeVersion: { policy: "appVersion" },
