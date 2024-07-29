ed50f1063fa2455ba245bb0c3c0001eb999810ac
Pavlos Vinieratos <pvinis@gmail.com>
2024-07-29 20:16:17 +0300

    wip

--- player/src/utils/dbQueries.ts
+++ player/src/utils/dbQueries.ts
@@ -3,11 +3,16 @@ import { db } from "./db"
 import { getLID } from "./lang"
 
 export async function getHangmanByDate(date: DateString) {
-	let { data } = await db
-		.from(`hangman-${getLID()}`)
-		.select("id, date, word")
-		.eq("date", date)
-		.throwOnError()
-		.single()
-	return data
+	try {
+		let { data } = await db
+			.from(`hangman-${getLID()}`)
+			.select("id, date, word")
+			.eq("date", date)
+			.throwOnError()
+			.single()
+
+		return data
+	} catch (e) {
+		console.error(e)
+	}
 }
