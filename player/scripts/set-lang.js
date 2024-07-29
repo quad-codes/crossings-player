1b50a62d3e70c2ed81e555277cbb6467556c8da8
Pavlos Vinieratos <pvinis@gmail.com>
2024-07-29 19:01:15 +0300

    aha

--- player/scripts/set-lang.js
+++ player/scripts/set-lang.js
@@ -2,21 +2,30 @@
 
 const fs = require("fs")
 const path = require("path")
+const jq = require("node-jq")
 
 const validLanguages = ["el", "he"]
 
-const lang = process.argv[2]
-if (!validLanguages.includes(lang)) {
-	console.error("Invalid language")
-	process.exit(1)
+async function main() {
+	const lang = process.argv[2]
+	if (!validLanguages.includes(lang)) {
+		console.error("Invalid language")
+		process.exit(1)
+	}
+
+	fs.copyFileSync(
+		path.join(__dirname, `../languages/assets/app-icon-${lang}.png`),
+		path.join(__dirname, "../assets/app-icon.png"),
+	)
+
+	fs.copyFileSync(
+		path.join(__dirname, `../languages/assets/splash-screen-${lang}.png`),
+		path.join(__dirname, "../assets/splash-screen.png"),
+	)
+
+	await jq.run('.id = "wow"', path.join(__dirname, "../extraConfig.json"))
 }
 
-fs.copyFileSync(
-	path.join(__dirname, `../languages/assets/app-icon-${lang}.png`),
-	path.join(__dirname, "../assets/app-icon.png"),
-)
-
-fs.copyFileSync(
-	path.join(__dirname, `../languages/assets/splash-screen-${lang}.png`),
-	path.join(__dirname, "../assets/splash-screen.png"),
-)
+;(async () => {
+	await main()
+})()
