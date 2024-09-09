68d4dc7198f6e9031a9b94967aea7fbd4d018415
Pavlos Vinieratos <pvinis@gmail.com>
2024-09-09 21:20:37 +0300

    try

---
+++ player/scripts/do-release.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process")
+const { diff, rsort } = require("semver")
+
+const tags = execSync("git tag").toString().split("\n")
+const versionTags = tags
+	.filter((tag) => tag.startsWith("version"))
+	.map((tag) => tag.replace("version/", ""))
+const sortedTags = rsort(versionTags)
+
+console.log(sortedTags)
+
+const currentDiff = diff(sortedTags[0], sortedTags[1])
+
+console.log(currentDiff)
+
+if (currentDiff === "major" || currentDiff === "minor") {
+	execSync("pnpm run build:prod")
+}
+if (currentDiff === "patch") {
+	execSync("pnpm run updatejs")
+}
---
+++ player/scripts/tag-rename.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+OLD_TAG=$(git describe --tags --abbrev=0)
+NEW_TAG=$(echo $OLD_TAG | sed 's/v/version\//')
+
+git tag $NEW_TAG $OLD_TAG
