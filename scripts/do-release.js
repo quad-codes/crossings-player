ef6e73970f41e18eb1eb8891ec6d9be0f8beb5fb
Pavlos Vinieratos <pvinis@gmail.com>
2024-09-15 19:06:14 +0300

    aha

--- scripts/do-release.js
+++ scripts/do-release.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const DEBUG = false
+
 const { execSync } = require("child_process")
 const { diff, rsort } = require("semver")
 
@@ -16,8 +18,8 @@ const currentDiff = diff(sortedTags[0], sortedTags[1])
 console.log(currentDiff)
 
 if (currentDiff === "major" || currentDiff === "minor") {
-	execSync("pnpm run build:prod")
+	execSync("pnpm run build:prod", { stdio: DEBUG ? "inherit" : undefined })
 }
 if (currentDiff === "patch") {
-	execSync("pnpm run updatejs")
+	execSync("pnpm run updatejs testtt", { stdio: DEBUG ? "inherit" : undefined })
 }
