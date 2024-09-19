b1ccc0f4a68015cec724d33c9a1e8b45f53ea09a
Pavlos Vinieratos <pvinis@gmail.com>
2024-09-19 17:37:24 +0300

    aha

--- scripts/do-release.js
+++ scripts/do-release.js
@@ -16,7 +16,7 @@ const currentDiff = diff(sortedTags[0], sortedTags[1])
 console.log(currentDiff)
 
 if (currentDiff === "major" || currentDiff === "minor") {
-	execSync("pnpm run build:prod -p ios", { stdio: "inherit" })
+	execSync("pnpm run build:prod --no-wait -p ios", { stdio: "inherit" })
 }
 if (currentDiff === "patch") {
 	execSync("pnpm run updatejs testtt", { stdio: "inherit" })
